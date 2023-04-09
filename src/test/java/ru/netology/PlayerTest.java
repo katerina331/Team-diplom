b700bab987700f4896ecc927b408a9cb7696d21c
Sergey <zhuravlevsergey7@gmail.com>
2023-04-09 17:33:39 +0300

    Added test for coverage

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -59,6 +59,25 @@ public class PlayerTest {
         Game actual = player.mostPlayerByGenre("Симулятор");
         assertEquals(expected, actual);
     }
+    @Test
+    public void shouldNotSumTimeWithNegativeValue() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Полет", "Симулятор");
+        Game game2 = store.publishGame("Гонки", "Симулятор");
+        Game game3 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.play(game1, 5);
+        player.play(game2, -10);
+        player.play(game3, 3);
+
+        Game expected = game1;
+        Game actual = player.mostPlayerByGenre("Симулятор");
+        assertEquals(expected, actual);
+    }
 
     @Test
     public void addGameWhenGameAlreadyInstalled() {
